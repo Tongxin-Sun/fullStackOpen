6e1775ad3e3bcd700e405611206ea16379466079
Tongxin Sun <tongxinsun16@gmail.com>
2025-10-29 17:18:43 -0700

    connecting backend to mongodb

---
+++ part3/phonebookBackend/mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstackopen:${password}@cluster0.5tbsnnk.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    Person.find({}).then(persons => {
+        persons.forEach(p => console.log(p))
+        mongoose.connection.close()
+    })
+} else {
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+
+    person.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+{/*note.save().then(result => {
+    console.log(`result: ${result}`)
+    console.log('note saved!')
+    mongoose.connection.close()
+})*/}
+{/*
+Note.find({ important: true }).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})*/}
