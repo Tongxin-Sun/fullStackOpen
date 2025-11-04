7aaee55fea04b5290106284a2d95a428c24ed744
Tongxin Sun <tongxinsun16@gmail.com>
2025-11-04 15:59:25 -0800

    Part3 completed

--- part3/demo/mongo.js
+++ part3/demo/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -14,26 +14,15 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+  content: String,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-})
-
-{/*note.save().then(result => {
-    console.log(`result: ${result}`)
-    console.log('note saved!')
-    mongoose.connection.close()
-})*/}
-
 Note.find({ important: true }).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
