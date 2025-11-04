e94afe7ee15a7c5074d238df9dd862633dd27f10
Tongxin Sun <tongxinsun16@gmail.com>
2025-11-04 15:59:01 -0800

    Part 3 completed

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -8,114 +8,93 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 const app = express()
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
+  console.log(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).send({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('dist'))
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 // Helpers
 const bodyRequiredFieldErrorHandler = (requiredFields, body, response) => {
-    const missingFields = requiredFields.filter(field => !body[field])
-    if (missingFields.length > 0) {
-        return response.status(400).json({
-            error: `Missing required field(s): ${missingFields.join(',')}`
-        })
-    }
+  const missingFields = requiredFields.filter(field => !body[field])
+  if (missingFields.length > 0) {
+    return response.status(400).json({
+      error: `Missing required field(s): ${missingFields.join(',')}`
+    })
+  }
 }
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => response.json(persons))
+  Person.find({}).then(persons => response.json(persons))
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => response.json(person))
+  Person.findById(request.params.id).then(person => response.json(person))
 
 })
 
 app.get('/info', (request, response) => {
-    const current = new Date()
-
-    response.send(
+  const current = new Date()
+  Person.find({})
+    .then(persons =>
+      response.send(
         `<p>Phonebook has info for ${persons.length} people</p>
          <p>${current}</p>`
+      )
     )
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    Person.findByIdAndDelete(id)
-        .then(deletedPerson => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  const id = request.params.id
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    bodyRequiredFieldErrorHandler(['name', 'number'], body, response)
+  bodyRequiredFieldErrorHandler(['name', 'number'], body, response)
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-    person.save()
-        .then(savedPerson => response.json(savedPerson))
-        .catch(error => next(error))
+  person.save()
+    .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    bodyRequiredFieldErrorHandler(['name', 'number'], body, response)
-    const opts = { runValidators: true }
-    Person.updateOne({ _id: request.params.id, name: body.name }, { $set: { number: body.number } }, opts)
-        .then(() => response.json(
-            {
-                name: body.name,
-                number: body.number
-            }
-        ))
+  bodyRequiredFieldErrorHandler(['name', 'number'], body, response)
+  const opts = { runValidators: true }
+  Person.updateOne({ _id: request.params.id, name: body.name }, { $set: { number: body.number } }, opts)
+    .then(() => response.json(
+      {
+        name: body.name,
+        number: body.number
+      }
+    ))
 
 
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
@@ -123,5 +102,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
