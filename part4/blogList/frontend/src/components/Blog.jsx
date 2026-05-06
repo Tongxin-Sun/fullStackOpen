64d2b99dfa80c24f1c3ad5cd35b678c8287f29fa
Tongxin Sun <tongxinsun16@gmail.com>
2026-05-05 18:21:51 -0700

    Complete exercise 5.7
    Exercise 5.7: Blog List Frontend
    
    Adding a button to each blog, which controls whether all of the details about the blog are shown or not.

--- part4/blogList/frontend/src/components/Blog.jsx
+++ part4/blogList/frontend/src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false)
+  const hide = { display: showAll ? '' : 'none' }
+  const label = showAll ? 'hide' : 'view'
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} <button onClick={() => setShowAll(!showAll)}>{label}</button>
+      <div style={hide}>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>like</button></div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
